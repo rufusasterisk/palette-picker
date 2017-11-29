08907f21ee1f8dcc99e53b18bc3e0719bfa3d51f
Rufus <rufusasterisk@gmail.com>
2017-11-29 13:40:30 -0700

    Add tetra color to scripts, add knex config and initial migration

--- server.js
+++ server.js
@@ -9,9 +9,13 @@ app.locals.title = 'Palette Picker';
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+const environment = process.env.NODE_ENV || 'development';
+const configuration = require('./knexfile')[environment];
+const database = require('knex')(configuration);
+
 app.use(express.static(__dirname + '/public'));
 
-app.set('port', process.env.PORT || 3000);
+app.set('port', process.env.PORT || 3002);
 
 app.listen(app.get('port'), () => {
   console.log(`${app.locals.title} is running on port ${app.get('port')}.`);
