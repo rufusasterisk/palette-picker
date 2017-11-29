a1c6eff5f11927b7ec574b0e205b36b0be448d36
Rufus <rufusasterisk@gmail.com>
2017-11-29 14:05:48 -0700

    Add migrations, add seeds

--- db/migrations/20171129132827_initial.js
+++ db/migrations/20171129132827_initial.js
@@ -4,6 +4,7 @@ exports.up = function(knex, Promise) {
     knex.schema.createTable('projects', function(table) {
       table.increments('id').primary();
       table.string('name');
+
       table.timestamps(true, true);
     }),
     knex.schema.createTable('palettes', function(table) {
@@ -24,7 +25,7 @@ exports.up = function(knex, Promise) {
 
 exports.down = function(knex, Promise) {
   return Promise.all([
-    knex.schema.dropTable('palettes');
-    knex.schema.dropTable('projects');
+    knex.schema.dropTable('palettes'),
+    knex.schema.dropTable('projects')
   ])
 };
