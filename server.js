fcef784a3374e87b4d2d0e85e85f0d0b12863c3f
Rufus <rufusasterisk@gmail.com>
2017-11-28 19:52:02 -0700

    Add post commands to server

--- server.js
+++ server.js
@@ -2,21 +2,20 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 
+app.locals.projects = [];
+app.locals.palettes = [];
+app.locals.title = 'Palette Picker';
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-
 
 app.use(express.static(__dirname + '/public'));
 
 app.set('port', process.env.PORT || 3000);
-app.locals.title = 'Palette Picker';
 
 app.listen(app.get('port'), () => {
   console.log(`${app.locals.title} is running on port ${app.get('port')}.`);
 })
-
-app.locals.projects = [];
-app.locals.palettes = [];
 
 app.get('/api/v1/projects', (request, response) => {
   return response.status(200).json(app.locals.projects);
@@ -28,7 +27,36 @@ app.get('/api/v1/palettes', (request, response) => {
 });
 
 const filterPalettes = (projectID) => {
-    return app.locals.palettes.filter( (palette) => {
-      palette.projectID === projectID;
-    });
+  return app.locals.palettes.filter( (palette) => {
+    palette.projectID === projectID;
+  });
 }
+
+app.post('/api/v1/projects', (request, response) => {
+  const { name } = request.params;
+  app.locals.projects.push({id: Date.now(), name: name});
+  return response.sendStatus(204);
+});
+
+app.post('/api/v1/palettes', (request, response) => {
+  const {
+    name,
+    color1,
+    color2,
+    color3,
+    color4,
+    color5,
+    projectID } = request.params;
+
+  app.locals.palettes.push({
+    id: Date.now(),
+    name: name,
+    color1: color1,
+    color2: color2,
+    color3: color3,
+    color4: color4,
+    color5: color5,
+    projectID: projectID
+  });
+  return response.sendStatus(204);
+});
